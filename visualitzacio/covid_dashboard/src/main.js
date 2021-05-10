cb946ada198faec3b63fbf06c76987972aa0082e
Yolanda Robla <yroblamo@redhat.com>
2021-05-10 22:11:26 +0200

    Added new version of app

--- visualitzacio/covid_dashboard/src/main.js
+++ visualitzacio/covid_dashboard/src/main.js
@@ -1,6 +1,5 @@
-// Import the dependencies and necessary modules
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
 import FusionCharts from 'fusioncharts';
 import Charts from 'fusioncharts/fusioncharts.charts';
 import Widgets from 'fusioncharts/fusioncharts.widgets';
@@ -8,6 +7,8 @@ import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import VueFusionCharts from 'vue-fusioncharts';
 import VuePapaParse from 'vue-papa-parse'
+
+Vue.config.productionTip = false
 
 // Resolve the dependencies
 Charts(FusionCharts);
@@ -18,20 +19,17 @@ FusionTheme(FusionCharts);
 // filter
 var numeral = require("numeral");
 Vue.filter("formatNumber", function(value) {
-	return numeral(value).format("0,0");
+        return numeral(value).format("0,0");
 });
 
 Vue.filter("formatTpc", function(value) {
-	return numeral(value).format("0.00");
+        return numeral(value).format("0.00");
 });
 
 // Globally register the components for project-wide use
 Vue.use(VueFusionCharts, FusionCharts);
 Vue.use(VuePapaParse);
 
-// Instantiate the Vue instance that controls the application
 new Vue({
- el: '#app',
- render: h => h(App)
-})
-
+  render: h => h(App),
+}).$mount('#app')
